25cba5f1508c6d5002d402632c7781fb503b90af
Evodiatembi <evodyembi@gmail.com>
2024-04-21 20:32:08 +0000

    models/engine/file_storage.py, models/engine/db_storage.py, models/state.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,3 +59,7 @@ class FileStorage:
             key = "{}.{}".format(type(obj).__name__, obj.id)
         if key in self.__objects:
             del self.__objects[key]
+
+    def close(self):
+         """ calls the reload method"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -11,3 +11,20 @@ class State(BaseModel):
     name = ""
     __tablename__ = "states"
     name = Column(String(128), null=None)
+if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """Get a list of City instances with
+                state_id equals to the current State.id.
+
+            This is a getter attribute for FileStorage
+                relationship between State and City.
+            """
+            city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
